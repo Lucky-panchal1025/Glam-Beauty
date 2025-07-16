e2210d807c2c2a39bdfcb386a42c8bc3493a4aaa
Lucky-panchal1025 <panchalucky65@gmail.com>
2025-07-16 17:42:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// 3 seconds baad splash screen hatake main content dikhana
+setTimeout(() => {
+  document.getElementById("splash-screen").style.display = "none";
+  document.getElementById("main-content").style.display = "block";
+}, 3000); // 3000 ms = 3 seconds
